6d6565bdebd175ed6a2029d47d9b720d19a12698
Vinícius Rodrigues dos Santos <vinicius@hotmail.com.br>
2015-09-11 23:22:21 -0300

    Files

---
+++ Source/MACaddress/MACaddress.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "MACaddress.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MACaddress, "MACaddress" );
---
+++ Source/MACaddress/MyActor.h
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/Actor.h"
+#include "WindowsPlatformMisc.h"
+#include "MyActor.generated.h"
+
+UCLASS()
+class MACADDRESS_API AMyActor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AMyActor();
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	
+	// Called every frame
+	virtual void Tick( float DeltaSeconds ) override;
+
+	UFUNCTION(BlueprintCallable, Category = "Seila")
+	FString GetMacAddress();
+	
+};
